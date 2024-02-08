d51a5f20a7b857e483f64017b8bd596763a85157
PasqualeIannace <pasquale.iannace@gmail.com>
2024-02-08 16:49:51 +0100

    Stilizzo gli eventi nelle card

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -17,9 +17,19 @@ export default {
 <template>
     <h1>Tutti gli eventi</h1>
 
-    <div>
-        <div v-for="evento in store.events">
-            {{ evento.name }}
+    <div class="row">
+        <div v-for="evento in  store.events " class="col-4">
+            <div class="card mb-3">
+                <img :src="evento.image" class="card-img-top " alt="...">
+                <div class="card-body">
+                    <h5 class="card-title"><strong> {{ evento.name }} </strong></h5>
+                    <p class="card-text">{{ evento.location }}</p>
+                    <p class="card-text"><strong>Data evento</strong> {{ evento.date }}</p>
+                    <span class="d-flex gap-2 ">
+                        <!-- <a href="{{route('admin.events.show', $event->id)}}" class="btn btn-primary ">Dettagli</a> -->
+                    </span>
+                </div>
+            </div>
         </div>
     </div>
 </template>
