4ffd5e900290f409446acec637274db45e96bff4
PasqualeIannace <pasquale.iannace@gmail.com>
2024-02-11 11:23:12 +0100

    Recupero singolo evento tramite chiamata axios

--- src/pages/EventDetail.vue
+++ src/pages/EventDetail.vue
@@ -1,6 +1,9 @@
 <script>
 import { store } from "../store.js" //state management
 
+import axios from 'axios'; //importo Axios
+
+
 export default {
     props: ["id"],
 
@@ -15,15 +18,15 @@ export default {
     },
 
     mounted() {
-        // axios.get(`${this.store.apiUrl}/api/events/${this.$route.params.id}`).then((response) => {
-        //     console.log("axios di:");
-        //     if (response.data.success) {
-        //         this.post = response.data.post;
-        //     } else {
-        //         // redirect alla pagina 404
-        //         this.$router.push({ name: 'not-found' })
-        //     }
-        // });
+        axios.get(`${this.store.apiUrl + this.store.apiEvent + this.id}`).then((response) => {
+            console.log("axios di:", response);
+            if (response.data.success) {
+                this.post = response.data.post;
+            } else {
+                // redirect alla pagina 404
+                this.$router.push({ name: 'not-found' })
+            }
+        });
     },
 }
 </script>
