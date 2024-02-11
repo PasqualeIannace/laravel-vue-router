d7bc39a98d98f6961e07460968ba047a17b3717e
PasqualeIannace <pasquale.iannace@gmail.com>
2024-02-11 11:09:36 +0100

    Aggiunta props id evento

--- src/pages/EventDetail.vue
+++ src/pages/EventDetail.vue
@@ -1,12 +1,35 @@
 <script>
-export default {
-    name: "EventDetail"
-}
+import { store } from "../store.js" //state management
 
+export default {
+    props: ["id"],
+
+    components: {
+        name: "EventDetail",
+    },
+
+    data() {
+        return {
+            store
+        }
+    },
+
+    mounted() {
+        // axios.get(`${this.store.apiUrl}/api/events/${this.$route.params.id}`).then((response) => {
+        //     console.log("axios di:");
+        //     if (response.data.success) {
+        //         this.post = response.data.post;
+        //     } else {
+        //         // redirect alla pagina 404
+        //         this.$router.push({ name: 'not-found' })
+        //     }
+        // });
+    },
+}
 </script>
 
 <template>
-    <h1>Dettagli evento</h1>
+    <h1>Dettagli evento {{ id }}</h1>
 </template>
 
 <style scoped></style>
--- src/router.js
+++ src/router.js
@@ -19,7 +19,8 @@ const router = createRouter({
         {
             path: '/evento-:id',
             name: 'evento',
-            component: EventDetail
+            component: EventDetail,
+            props: true
         },
     ]
 });
